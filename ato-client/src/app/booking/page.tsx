94a9263198bff765976c4013d8392566b7755c25
thangnv <tidihoc123@gmail.com>
2025-03-06 16:09:51 +0700

    order

---
+++ ato-client/src/app/booking/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: " Page ",
+  description: "This is a Page ",
+  // other metadata
+};
+
+const Page = () => {
+  return;
+  <></>;
+};
+
+export default Page;
