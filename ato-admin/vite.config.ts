1fc6c64d244c4c4bd618ebffcd9a176d15d5927a
Hoang-N-D-1 <ngusimeri1@gmail.com>
2025-03-11 14:50:34 +0700

    fix-be-connect

--- ato-admin/vite.config.ts
+++ ato-admin/vite.config.ts
@@ -1,34 +1,44 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    global: 'window'
-  },
-  resolve: {
-    alias: [
-      {
-        find: /^~(.+)/,
-        replacement: path.join(process.cwd(), 'node_modules/$1')
-      },
-      {
-        find: /^src(.+)/,
-        replacement: path.join(process.cwd(), 'src/$1')
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    define: {
+      global: 'window'
+    },
+    resolve: {
+      alias: [
+        {
+          find: /^~(.+)/,
+          replacement: path.join(process.cwd(), 'node_modules/$1')
+        },
+        {
+          find: /^src(.+)/,
+          replacement: path.join(process.cwd(), 'src/$1')
+        }
+      ]
+    },
+    server: {
+      open: true,
+      port: 3000,
+      host: true, // needed for docker
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
-    ]
-  },
-  server: {
-    // this ensures that the browser opens upon server start
-    open: true,
-    // this sets a default port to 3000
-    port: 3000
-  },
-  preview: {
-    // this ensures that the browser opens upon preview start
-    open: true,
-    // this sets a default port to 3000
-    port: 3000
-  }
+    },
+    preview: {
+      open: true,
+      port: 3000
+    }
+  };
 });
