77615263bb2855fe2b40adb9ddd03c2ed74edaeb
Hoang-N-D-1 <ngusimeri1@gmail.com>
2025-03-16 12:52:43 +0700

    create-detail-product

--- ato-admin/src/pages/tourism-facility/product/create.tsx
+++ ato-admin/src/pages/tourism-facility/product/create.tsx
@@ -1,5 +1,296 @@
-const Page = () => {
-  return <></>;
+import { LoadingButton } from '@mui/lab';
+import { Box, Button, Card, CardContent, Divider, Grid, MenuItem, Stack, TextField, Typography } from '@mui/material';
+import { Formik } from 'formik';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
+import { MultipleFileUploader } from '../../../components/upload/MultipleFileUploader';
+import { TOURISM_FACILITY_URLs } from '../../../constants/tourism-facility-urls';
+import { createProduct } from '../../../redux/tourism-facility/product.slice';
+import { ProductCategory, ProductCategoryLabels } from '../../../types/tourism-facility/product-category.enum';
+import { TCreateProduct } from '../../../types/tourism-facility/product.types';
+
+const validationSchema = Yup.object().shape({
+  productName: Yup.string().required('Product name is required'),
+  imgs: Yup.array().min(1, 'At least one image is required'),
+  description: Yup.string().required('Description is required'),
+  manufacturer: Yup.string().required('Manufacturer is required'),
+  addressManufacturer: Yup.string().required('Manufacturer address is required'),
+  unitProduct: Yup.number()
+    .required('Unit product is required')
+    .min(0, 'Unit product must be positive')
+    .typeError('Unit product must be a number'),
+  productCategory: Yup.number().required('Product category is required').typeError('Product category must be a number')
+});
+
+const initialValues: TCreateProduct = {
+  productName: '',
+  imgs: [],
+  description: '',
+  additional: '',
+  nutritionType: '',
+  age: '',
+  ingredient: '',
+  volume: '',
+  origin: '',
+  manufacturer: '',
+  addressManufacturer: '',
+  unitProduct: 0,
+  productCategory: ProductCategory.Food // Set default category
 };
 
-export default Page;
+const CreateProduct = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch<any>();
+
+  const handleSubmit = async (values: TCreateProduct, { setSubmitting }: any) => {
+    console.log('here');
+    try {
+      console.log('here');
+      await dispatch(createProduct(values)).unwrap();
+      navigate(TOURISM_FACILITY_URLs.PRODUCT.INDEX);
+    } catch (error) {
+      console.error('Failed to create product:', error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant="h4" textAlign={'center'} gutterBottom>
+          Tạo mới sản phẩm
+        </Typography>
+
+        <Formik onSubmit={handleSubmit} initialValues={initialValues} validationSchema={validationSchema}>
+          {({ values, errors, touched, handleChange, handleBlur, isSubmitting, handleSubmit, setFieldValue }) => (
+            <form noValidate onSubmit={handleSubmit}>
+              <Grid container spacing={3}>
+                {/* Product Images */}
+                <Grid item xs={12}>
+                  <Typography variant="h6" gutterBottom>
+                    Hình ảnh sản phẩm
+                  </Typography>
+                  <MultipleFileUploader values={values.imgs} onChange={(urls) => setFieldValue('imgs', urls)} />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Divider />
+                </Grid>
+
+                {/* Basic Information */}
+                <Grid item xs={12}>
+                  <Typography variant="h6" gutterBottom>
+                    Thông tin cơ bản
+                  </Typography>
+                </Grid>
+
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    fullWidth
+                    name="productName"
+                    label="Tên sản phẩm"
+                    value={values.productName}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.productName && Boolean(errors.productName)}
+                    helperText={touched.productName && errors.productName}
+                    InputLabelProps={{ shrink: true }}
+                  />
+                </Grid>
+
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    select
+                    fullWidth
+                    name="productCategory"
+                    label="Danh mục"
+                    value={values.productCategory}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.productCategory && Boolean(errors.productCategory)}
+                    helperText={touched.productCategory && errors.productCategory}
+                    InputLabelProps={{ shrink: true }}
+                  >
+                    {Object.entries(ProductCategoryLabels).map(([value, label]) => (
+                      <MenuItem key={value} value={Number(value)}>
+                        {label}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                </Grid>
+
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    fullWidth
+                    name="unitProduct"
+                    label="Số lượng"
+                    type="number"
+                    value={values.unitProduct}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.unitProduct && Boolean(errors.unitProduct)}
+                    helperText={touched.unitProduct && errors.unitProduct}
+                    InputLabelProps={{ shrink: true }}
+                  />
+                </Grid>
+
+                {/* Manufacturer Information */}
+                <Grid item xs={12}>
+                  <Divider />
+                  <Box sx={{ mt: 2 }}>
+                    <Typography variant="h6" gutterBottom>
+                      Thông tin nhà sản xuất
+                    </Typography>
+                  </Box>
+                </Grid>
+
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    fullWidth
+                    name="manufacturer"
+                    label="Nhà sản xuất"
+                    value={values.manufacturer}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.manufacturer && Boolean(errors.manufacturer)}
+                    helperText={touched.manufacturer && errors.manufacturer}
+                    InputLabelProps={{ shrink: true }}
+                  />
+                </Grid>
+
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    fullWidth
+                    name="addressManufacturer"
+                    label="Địa chỉ nhà sản xuất"
+                    value={values.addressManufacturer}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.addressManufacturer && Boolean(errors.addressManufacturer)}
+                    helperText={touched.addressManufacturer && errors.addressManufacturer}
+                    InputLabelProps={{ shrink: true }}
+                  />
+                </Grid>
+
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    fullWidth
+                    name="origin"
+                    label="Nguồn gốc"
+                    value={values.origin}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    InputLabelProps={{ shrink: true }}
+                  />
+                </Grid>
+
+                {/* Product Details */}
+                <Grid item xs={12}>
+                  <Divider />
+                  <Box sx={{ mt: 2 }}>
+                    <Typography variant="h6" gutterBottom>
+                      Chi tiết sản phẩm
+                    </Typography>
+                  </Box>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    multiline
+                    rows={4}
+                    name="description"
+                    label="Mô tả"
+                    value={values.description}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.description && Boolean(errors.description)}
+                    helperText={touched.description && errors.description}
+                    InputLabelProps={{ shrink: true }}
+                  />
+                </Grid>
+
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    fullWidth
+                    name="nutritionType"
+                    label="Loại dinh dưỡng"
+                    value={values.nutritionType}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    InputLabelProps={{ shrink: true }}
+                  />
+                </Grid>
+
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    fullWidth
+                    name="age"
+                    label="Tuổi"
+                    value={values.age}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    InputLabelProps={{ shrink: true }}
+                  />
+                </Grid>
+
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    fullWidth
+                    name="ingredient"
+                    label="Thành phần"
+                    value={values.ingredient}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    InputLabelProps={{ shrink: true }}
+                  />
+                </Grid>
+
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    fullWidth
+                    name="volume"
+                    label="Khối lượng"
+                    value={values.volume}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    InputLabelProps={{ shrink: true }}
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    multiline
+                    rows={4}
+                    name="additional"
+                    label="Thông tin thêm"
+                    value={values.additional}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    InputLabelProps={{ shrink: true }}
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Stack direction="row" spacing={2} justifyContent="flex-end">
+                    <Button variant="outlined" onClick={() => navigate(TOURISM_FACILITY_URLs.PRODUCT.INDEX)}>
+                      Hủy
+                    </Button>
+                    <LoadingButton variant="contained" type="submit" loading={isSubmitting}>
+                      Tạo sản phẩm
+                    </LoadingButton>
+                  </Stack>
+                </Grid>
+              </Grid>
+            </form>
+          )}
+        </Formik>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CreateProduct;
--- ato-admin/src/pages/tourism-facility/product/index.tsx
+++ ato-admin/src/pages/tourism-facility/product/index.tsx
@@ -1,5 +1,158 @@
-const Page = () => {
-  return <></>;
+import {
+  Button,
+  Card,
+  CardContent,
+  Chip,
+  IconButton,
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TextField,
+  Typography
+} from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { TOURISM_FACILITY_URLs } from '../../../constants/tourism-facility-urls';
+import type { RootState } from '../../../redux/store';
+import { fetchProducts } from '../../../redux/tourism-facility/product.slice';
+import { EditOutlined, EyeOutlined, PlusOutlined, SearchOutlined } from '@ant-design/icons';
+import { NoDataDisplay } from '../../../components/no-data/NoDataDisplay';
+import AppSearchBar from '../../../components/table/SearchBar';
+import { ProductCategory, ProductCategoryLabels } from '../../../types/tourism-facility/product-category.enum';
+
+const ProductList = () => {
+  const dispatch = useDispatch<any>();
+  const navigate = useNavigate();
+  const { products } = useSelector((state: RootState) => state.productSlice);
+  const [searchText, setSearchText] = useState('');
+  const [page, setPage] = useState(0); // MUI pagination starts from 0
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (_: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  // Filter products based on search text
+  const filteredProducts =
+    products?.filter(
+      (product) =>
+        product.productName.toLowerCase().includes(searchText.toLowerCase()) ||
+        product.manufacturer.toLowerCase().includes(searchText.toLowerCase()) ||
+        product.origin.toLowerCase().includes(searchText.toLowerCase())
+    ) || [];
+
+  // Get current page products
+  const currentProducts = filteredProducts.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  return (
+    <Stack spacing={2}>
+      <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
+        <AppSearchBar placeholder="Tìm kiếm sản phẩm" onChange={(e) => setSearchText(e.target.value)} />
+        <Button variant="contained" startIcon={<PlusOutlined />} onClick={() => navigate(TOURISM_FACILITY_URLs.PRODUCT.CREATE)}>
+          Thêm sản phẩm
+        </Button>
+      </Stack>
+
+      <Card>
+        {currentProducts?.length > 0 && (
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell width={500}>Tên sản phẩm</TableCell>
+                  <TableCell>Nhà sản xuất</TableCell>
+                  <TableCell>Nguồn gốc</TableCell>
+                  <TableCell>Số lượng</TableCell>
+                  <TableCell>Danh mục</TableCell>
+                  <TableCell align="right">Hành động</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {currentProducts?.map((product) => (
+                  <TableRow key={product.productId}>
+                    <TableCell>
+                      <Typography
+                        sx={{
+                          maxWidth: 500,
+                          overflow: 'hidden',
+                          textOverflow: 'ellipsis',
+                          whiteSpace: 'nowrap'
+                        }}
+                      >
+                        {product.productName}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography
+                        sx={{
+                          maxWidth: 150,
+                          overflow: 'hidden',
+                          textOverflow: 'ellipsis',
+                          whiteSpace: 'nowrap'
+                        }}
+                      >
+                        {product.manufacturer}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>{product.origin}</TableCell>
+                    <TableCell>{product.unitProduct}</TableCell>
+                    <TableCell>
+                      <Chip label={ProductCategoryLabels[product.productCategory as ProductCategory]} color="primary" size="small" />
+                    </TableCell>
+                    <TableCell align="right">
+                      <Stack direction="row" spacing={1} justifyContent="flex-end">
+                        <IconButton
+                          size="small"
+                          onClick={() => navigate(`${TOURISM_FACILITY_URLs.PRODUCT.DETAILS}?id=${product.productId}`)}
+                        >
+                          <EyeOutlined />
+                        </IconButton>
+                        <IconButton
+                          size="small"
+                          onClick={() => navigate(`${TOURISM_FACILITY_URLs.PRODUCT.UPDATE}?id=${product.productId}`)}
+                        >
+                          <EditOutlined />
+                        </IconButton>
+                      </Stack>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 100]}
+              component="div"
+              count={filteredProducts.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              labelRowsPerPage="Số hàng mỗi trang:"
+              labelDisplayedRows={({ from, to, count }) => `${from}-${to} trên ${count}`}
+            />
+          </TableContainer>
+        )}
+
+        {!currentProducts && <NoDataDisplay />}
+      </Card>
+    </Stack>
+  );
 };
 
-export default Page;
+export default ProductList;
