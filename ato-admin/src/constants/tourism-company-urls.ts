64148aac2dfe3f7852befb04d690201e2fcdf236
thangnv <tidihoc123@gmail.com>
2025-03-17 14:48:07 +0700

    constants

--- ato-admin/src/constants/tourism-company-urls.ts
+++ ato-admin/src/constants/tourism-company-urls.ts
@@ -13,5 +13,18 @@ export const TOURISM_COMPANY_URLs = {
     UPDATE: `${TOURISM_COMPANY_BASE_URL}/tour-guide-team/update`,
     DETAILS: `${TOURISM_COMPANY_BASE_URL}/tour-guide-team/details`,
     REQUEST: `${TOURISM_COMPANY_BASE_URL}/tour-guide-team/request`
+  },
+  TOUR_PACKAGE: {
+    INDEX: `${TOURISM_COMPANY_BASE_URL}/tour-package`,
+    CREATE: `${TOURISM_COMPANY_BASE_URL}/tour-package/create`,
+    UPDATE: `${TOURISM_COMPANY_BASE_URL}/tour-package/update`,
+    DETAILS: `${TOURISM_COMPANY_BASE_URL}/tour-package/details`
+  },
+  TOUR_DESTINATION: {
+    CREATE: (packageId: number) => `${TOURISM_COMPANY_BASE_URL}/tour-package/destinations/create?packageId=${packageId}`,
+    UPDATE: (packageId: number, destinationId: number) =>
+      `${TOURISM_COMPANY_BASE_URL}/tour-package/destinations/update?packageId=${packageId}&destinationId=${destinationId}`,
+    DETAILS: (packageId: number, destinationId: number) =>
+      `${TOURISM_COMPANY_BASE_URL}/tour-package/destinations/details?packageId=${packageId}&destinationId=${destinationId}`
   }
 };
--- ato-admin/src/constants/tourism-facility-urls.ts
+++ ato-admin/src/constants/tourism-facility-urls.ts
@@ -5,6 +5,34 @@ export const TOURISM_FACILITY_URLs = {
     INDEX: `${TOURISM_FACILITY_BASE_URL}/product`,
     CREATE: `${TOURISM_FACILITY_BASE_URL}/product/create`,
     UPDATE: `${TOURISM_FACILITY_BASE_URL}/product/update`,
-    DETAILS: `${TOURISM_FACILITY_BASE_URL}/product/details`
+    DETAILS: `${TOURISM_FACILITY_BASE_URL}/product/details`,
+    CERTIFICATES: '/tourism-facility/product/certificates',
+    CREATE_CERTIFICATE: '/tourism-facility/product/certificates/create',
+    UPDATE_CERTIFICATE: '/tourism-facility/product/certificates/update',
+    VIEW_CERTIFICATE: '/tourism-facility/product/certificates/view'
+  },
+  PACKAGE: {
+    INDEX: `${TOURISM_FACILITY_BASE_URL}/package`,
+    CREATE: `${TOURISM_FACILITY_BASE_URL}/package/create`,
+    UPDATE: `${TOURISM_FACILITY_BASE_URL}/package/update`,
+    DETAILS: `${TOURISM_FACILITY_BASE_URL}/package/details`,
+    ACTIVITY: {
+      INDEX: (packageId: number) => `${TOURISM_FACILITY_BASE_URL}/package/activities?packageId=${packageId}`,
+      CREATE: (packageId: number) => `${TOURISM_FACILITY_BASE_URL}/package/activities/create?packageId=${packageId}`,
+      UPDATE: (packageId: number, activityId: number) =>
+        `${TOURISM_FACILITY_BASE_URL}/package/activities/update?packageId=${packageId}&activityId=${activityId}`,
+      DETAILS: (packageId: number, activityId: number) =>
+        `${TOURISM_FACILITY_BASE_URL}/package/activities/view?packageId=${packageId}&activityId=${activityId}`
+    }
+  },
+  ORDER: {
+    INDEX: `${TOURISM_FACILITY_BASE_URL}/orders`,
+    DETAILS: `${TOURISM_FACILITY_BASE_URL}/orders/details`
+  },
+  CONTRACT: {
+    INDEX: `${TOURISM_FACILITY_BASE_URL}/contracts`,
+    CREATE: `${TOURISM_FACILITY_BASE_URL}/contracts/create`,
+    UPDATE: `${TOURISM_FACILITY_BASE_URL}/contracts/update`,
+    DETAILS: `${TOURISM_FACILITY_BASE_URL}/contracts/details`
   }
 };
