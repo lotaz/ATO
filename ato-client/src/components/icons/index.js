0ec01bcf3dbcb228244c84366a1adfd4772ae921
thangnv <tidihoc123@gmail.com>
2025-03-20 14:59:57 +0700

    icon-duotone

---
+++ ato-client/src/components/icons/index.js
@@ -0,0 +1,208 @@
+import AlarmClock from "./AlarmClock";
+import Apple from "./Apple";
+import AppleStore from "./AppleStore";
+import BabyBoy from "./BabyBoy";
+import BabyFeeder from "./BabyFeeder";
+import BabyGirl from "./BabyGirl";
+import BasketBall from "./BasketBall";
+import BazaarIcon from "./BazaarIcon";
+import BeautyProducts from "./BeautyProducts";
+import Beer from "./Beer";
+import Bottle from "./Bottle";
+import Breakfast from "./Breakfast";
+import Camera from "./Camera";
+import Car from "./Car";
+import Carrot from "./Carrot";
+import CartIcon from "./Cart";
+import CartCheck from "./CartCheck";
+import CartX from "./CartX";
+import Category from "./Category";
+import CategoryOutlined from "./CategoryOutline";
+import CreditCardVerified from "./CreditCardVerified";
+import CustomerService from "./CustomerService";
+import Delivery from "./Delivery";
+import DeliveryBox from "./DeliveryBox";
+import DottedStar from "./DottedStar";
+import Dress from "./Dress";
+import DrillMachine from "./DrillMachine";
+import Eye from "./Eye";
+import Facebook from "./Facebook";
+import FacebookFilled from "./FacebookFilled";
+import FeedbackThumbsUp from "./FeedbackThumbsUp";
+import Figma from "./Figma";
+import Food from "./Food";
+import Gift from "./Gift";
+import GiftBox from "./GiftBox";
+import Globe from "./Globe";
+import GoldPackageIcon from "./GoldPackageIcon";
+import Google from "./Google";
+import Heart from "./Heart";
+import Honey from "./Honey";
+import Instagram from "./Instagram";
+import InstagramFilled from "./InstagramFilled";
+import Laptop from "./Laptop";
+import Light from "./Light";
+import Lightning from "./Lightning";
+import MakeUp from "./MakeUp";
+import Man from "./Man";
+import Microphone from "./Microphone";
+import Milk from "./Milk";
+import MoneyGuarantee from "./MoneyGurantee";
+import MotorBike from "./MotorBike";
+import NewArrival from "./NewArrival";
+import OnlineService from "./OnlineService";
+import OnlineSupport from "./OnlineSupport";
+import PackageBox from "./PackageBox";
+import Payment from "./Payment";
+import Pen from "./Pen";
+import Pets from "./Pets";
+import PickUpBox from "./PickUpBox";
+import Picture from "./Picture";
+import PlantPot from "./PlantPot";
+import PlayStore from "./PlayStore";
+import PremiumPackageIcon from "./PremiumPackageIcon";
+import Preview from "./Preview";
+import Quote from "./Quote";
+import RankBadge from "./RankBadge";
+import Reload from "./Reload";
+import Shield from "./Shield";
+import ShoppingBagOutlined from "./ShoppingBagOutlined";
+import SilverPackageIcon from "./SilverPackageIcon";
+import Snack from "./Snack";
+import SofaTwo from "./Sofa";
+import Telegram from "./Telegram";
+import Toggle from "./Toggle";
+import Truck from "./Truck";
+import TruckFast from "./TruckFast";
+import TruckFilled from "./TruckFilled";
+import Tshirt from "./Tshirt";
+import Twitter from "./Twitter";
+import TwitterFilled from "./TwitterFilled";
+import UpDown from "./UpDown";
+import User2 from "./User2";
+import Verify from "./Verify";
+import Vest from "./Vest";
+import Watch from "./Watch";
+import Wheel from "./Wheel";
+import Woman from "./Woman";
+import WomenDress from "./WomenDress";
+import Yogurt from "./Yogurt";
+import Youtube from "./Youtube";
+import YoutubeFilled from "./YoutubeFilled"; // furniture shop page icons
+
+import Powder from "./health-and-beauty/Powder";
+import Lotion from "./health-and-beauty/Lotion";
+import Mascara from "./health-and-beauty/Mascara";
+import Lipstick from "./health-and-beauty/Lipstick";
+import FaceMask from "./health-and-beauty/FaceMask";
+import LenseCase from "./health-and-beauty/LenseCase";
+import AutoMotive from "./health-and-beauty/Automotive";
+import OxygenPump from "./health-and-beauty/OxygenPump";
+import Thermometer from "./health-and-beauty/Thermometer";
+import HealthBeauty from "./health-and-beauty/HealthBeauty";
+const appIcons = {
+  Beer,
+  Milk,
+  Snack,
+  Truck,
+  Honey,
+  Apple,
+  Shield,
+  Bottle,
+  Carrot,
+  Yogurt,
+  Payment,
+  PickUpBox,
+  Breakfast,
+  AlarmClock,
+  OnlineSupport,
+  OnlineService,
+  MoneyGuarantee,
+  CustomerService,
+  FeedbackThumbsUp,
+  CreditCardVerified,
+
+  // health-and-beauty-page-icons
+  Powder,
+  Lotion,
+  Mascara,
+  Lipstick,
+  FaceMask,
+  LenseCase,
+  OxygenPump,
+  AutoMotive,
+  Thermometer,
+  HealthBeauty,
+  Car,
+  Watch,
+  Wheel,
+  Camera,
+  Tshirt,
+  BabyBoy,
+  Picture,
+  BabyGirl,
+  WomenDress,
+  AppleStore,
+  BabyFeeder,
+  BasketBall,
+  BazaarIcon,
+  DrillMachine,
+  BeautyProducts,
+  CartIcon,
+  CartCheck,
+  CartX,
+  Category,
+  CategoryOutlined,
+  Delivery,
+  DeliveryBox,
+  DottedStar,
+  Dress,
+  Eye,
+  Facebook,
+  FacebookFilled,
+  Figma,
+  Food,
+  Gift,
+  Globe,
+  GoldPackageIcon,
+  Google,
+  GiftBox,
+  Heart,
+  Instagram,
+  InstagramFilled,
+  Laptop,
+  Light,
+  Lightning,
+  MakeUp,
+  Man,
+  Microphone,
+  MotorBike,
+  NewArrival,
+  PackageBox,
+  Pen,
+  Pets,
+  PlantPot,
+  PlayStore,
+  PremiumPackageIcon,
+  Preview,
+  Quote,
+  RankBadge,
+  Reload,
+  ShoppingBagOutlined,
+  SilverPackageIcon,
+  SofaTwo,
+  Telegram,
+  Toggle,
+  TruckFast,
+  TruckFilled,
+  Twitter,
+  TwitterFilled,
+  UpDown,
+  User2,
+  Verify,
+  Vest,
+  Woman,
+  Youtube,
+  YoutubeFilled,
+};
+export default appIcons;
