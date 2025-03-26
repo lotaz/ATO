3661c22670b8cf1a483201bca32647b36bfb6508
Hoang-N-D-1 <ngusimeri1@gmail.com>
2025-03-26 20:13:50 +0700

    accommodation-crud

--- ato-admin/src/menu-items/tourism-company-menu.tsx
+++ ato-admin/src/menu-items/tourism-company-menu.tsx
@@ -1,5 +1,5 @@
 // assets
-import { BoxPlotOutlined, TeamOutlined } from '@ant-design/icons';
+import { BoxPlotOutlined, TeamOutlined, CarOutlined, EnvironmentOutlined, HomeOutlined } from '@ant-design/icons';
 import { TOURISM_COMPANY_URLs } from '../constants/tourism-company-urls';
 import { TMenu } from './types';
 
@@ -7,29 +7,6 @@ const tourCompanuMenu: TMenu = {
   id: 'pages',
   type: 'group',
   children: [
-    {
-      id: 'tour-guide-team',
-      title: 'Quản lý hướng dẫn viên',
-      type: 'item',
-      url: TOURISM_COMPANY_URLs.TOUR_GUIDE_TEAM.INDEX,
-      icon: TeamOutlined,
-      subItems: [
-        {
-          id: 'create-tour-guide',
-          title: 'Thêm hướng dẫn viên',
-          type: 'item',
-          url: TOURISM_COMPANY_URLs.TOUR_GUIDE_TEAM.CREATE,
-          icon: TeamOutlined
-        },
-        {
-          id: 'request-tour-guide',
-          title: 'Yêu cầu làm hướng dẫn viên',
-          type: 'item',
-          url: TOURISM_COMPANY_URLs.TOUR_GUIDE_TEAM.REQUEST,
-          icon: TeamOutlined
-        }
-      ]
-    },
     {
       id: 'tour-package',
       title: 'Quản lý gói du lịch',
@@ -57,6 +34,119 @@ const tourCompanuMenu: TMenu = {
           type: 'item',
           url: TOURISM_COMPANY_URLs.TOUR_PACKAGE.UPDATE,
           icon: BoxPlotOutlined
+        }
+      ]
+    },
+    {
+      id: 'tour-guide-team',
+      title: 'Quản lý hướng dẫn viên',
+      type: 'item',
+      url: TOURISM_COMPANY_URLs.TOUR_GUIDE_TEAM.INDEX,
+      icon: TeamOutlined,
+      subItems: [
+        {
+          id: 'create-tour-guide',
+          title: 'Thêm hướng dẫn viên',
+          type: 'item',
+          url: TOURISM_COMPANY_URLs.TOUR_GUIDE_TEAM.CREATE,
+          icon: TeamOutlined
+        },
+        {
+          id: 'view-tour-guide',
+          title: 'Danh sách hướng dẫn viên',
+          type: 'item',
+          url: TOURISM_COMPANY_URLs.TOUR_GUIDE_TEAM.INDEX,
+          icon: TeamOutlined
+        }
+      ]
+    },
+    {
+      id: 'driver',
+      title: 'Quản lý tài xế',
+      type: 'item',
+      url: TOURISM_COMPANY_URLs.DRIVER.INDEX,
+      icon: CarOutlined,
+      subItems: [
+        {
+          id: 'create-driver',
+          title: 'Thêm tài xế',
+          type: 'item',
+          url: TOURISM_COMPANY_URLs.DRIVER.CREATE,
+          icon: CarOutlined
+        },
+        {
+          id: 'view-driver',
+          title: 'Chi tiết tài xế',
+          type: 'item',
+          url: TOURISM_COMPANY_URLs.DRIVER.DETAILS,
+          icon: CarOutlined
+        },
+        {
+          id: 'update-driver',
+          title: 'Cập nhật tài xế',
+          type: 'item',
+          url: TOURISM_COMPANY_URLs.DRIVER.UPDATE,
+          icon: CarOutlined
+        },
+        {
+          id: 'accommodation',
+          title: 'Quản lý nhà nghỉ',
+          type: 'item',
+          url: TOURISM_COMPANY_URLs.ACCOMMODATION.INDEX,
+          icon: HomeOutlined,
+          subItems: [
+            {
+              id: 'create-accommodation',
+              title: 'Thêm nhà nghỉ',
+              type: 'item',
+              url: TOURISM_COMPANY_URLs.ACCOMMODATION.CREATE,
+              icon: HomeOutlined
+            },
+            {
+              id: 'view-accommodation',
+              title: 'Chi tiết nhà nghỉ',
+              type: 'item',
+              url: TOURISM_COMPANY_URLs.ACCOMMODATION.DETAILS,
+              icon: HomeOutlined
+            },
+            {
+              id: 'update-accommodation',
+              title: 'Cập nhật nhà nghỉ',
+              type: 'item',
+              url: TOURISM_COMPANY_URLs.ACCOMMODATION.UPDATE,
+              icon: HomeOutlined
+            }
+          ]
+        }
+      ]
+    },
+    {
+      id: 'accommodation',
+      title: 'Quản lý nhà nghỉ',
+      type: 'item',
+      url: TOURISM_COMPANY_URLs.ACCOMMODATION.INDEX,
+      icon: HomeOutlined,
+      subItems: [
+        {
+          id: 'create-accommodation',
+          title: 'Thêm nhà nghỉ',
+          type: 'item',
+          url: TOURISM_COMPANY_URLs.ACCOMMODATION.CREATE,
+          icon: HomeOutlined
+        },
+        {
+          id: 'view-accommodation',
+          title: 'Chi tiết nhà nghỉ',
+          type: 'item',
+          url: TOURISM_COMPANY_URLs.ACCOMMODATION.DETAILS,
+          icon: HomeOutlined
+        },
+        {
+          id: 'update-accommodation',
+          title: 'Cập nhật nhà nghỉ',
+          type: 'item',
+          url: TOURISM_COMPANY_URLs.ACCOMMODATION.UPDATE,
+          icon: HomeOutlined
         }
       ]
     }
--- ato-admin/src/pages/tourism-company/accommodation/details.tsx
+++ ato-admin/src/pages/tourism-company/accommodation/details.tsx
@@ -5,49 +5,38 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import { TOURISM_COMPANY_URLs } from '../../../constants/tourism-company-urls';
 import { RootState } from '../../../redux/store';
-import { fetchDriver } from '../../../redux/tourism-company/driver.slice';
-import { VehicleType } from '../../../types/tourism-company/driver.types';
+import { fetchAccommodation } from '../../../redux/tourism-company/accommodation.slice';
 
-const DriverDetails = () => {
+const AccommodationDetails = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch<any>();
   const { id } = useParams();
-  const { specific: driver, loading } = useSelector((state: RootState) => state.driverSlice);
+  const { specific: accommodation, loading } = useSelector((state: RootState) => state.accommodationSlice);
 
   useEffect(() => {
     if (id) {
-      dispatch(fetchDriver(id));
+      dispatch(fetchAccommodation(id));
     }
   }, [dispatch, id]);
 
-  if (loading || !driver) {
+  if (loading || !accommodation) {
     return <Typography>Đang tải...</Typography>;
   }
-
-  const getVehicleTypeDisplay = (type: VehicleType) => {
-    const displayNames: Record<VehicleType, string> = {
-      [VehicleType.CAR_4]: 'Xe 4 chỗ',
-      [VehicleType.CAR_7]: 'Xe 7 chỗ',
-      [VehicleType.CAR_16]: 'Xe 16 chỗ',
-      [VehicleType.CAR_29]: 'Xe 29 chỗ',
-      [VehicleType.CAR_45]: 'Xe 45 chỗ',
-      [VehicleType.SLEEPER_BUS_SINGLE]: 'Xe giường nằm đơn',
-      [VehicleType.SLEEPER_BUS_COUPLE]: 'Xe giường nằm đôi',
-      [VehicleType.FLY]: 'Máy bay'
-    };
-    return displayNames[type] || type;
-  };
 
   return (
     <Stack spacing={3}>
       <Stack direction="row" alignItems="center" justifyContent="space-between" spacing={2}>
         <Stack direction="row" alignItems="center" spacing={2}>
-          <Button startIcon={<ArrowLeftOutlined />} onClick={() => navigate(TOURISM_COMPANY_URLs.DRIVER.INDEX)}>
+          <Button startIcon={<ArrowLeftOutlined />} onClick={() => navigate(TOURISM_COMPANY_URLs.ACCOMMODATION.INDEX)}>
             Quay lại danh sách
           </Button>
-          <Typography variant="h5">Chi tiết tài xế</Typography>
+          <Typography variant="h5">Chi tiết nhà nghỉ</Typography>
         </Stack>
-        <Button variant="contained" startIcon={<EditOutlined />} onClick={() => navigate(`${TOURISM_COMPANY_URLs.DRIVER.UPDATE}/${id}`)}>
+        <Button
+          variant="contained"
+          startIcon={<EditOutlined />}
+          onClick={() => navigate(`${TOURISM_COMPANY_URLs.ACCOMMODATION.UPDATE.replace(':id', id)}`)}
+        >
           Chỉnh sửa
         </Button>
       </Stack>
@@ -58,9 +47,9 @@ const DriverDetails = () => {
             <Grid item xs={12} md={6}>
               <Stack spacing={2}>
                 <Typography variant="subtitle1" fontWeight="bold">
-                  Tên tài xế
+                  Tên nhà nghỉ
                 </Typography>
-                <Typography>{driver.driverName}</Typography>
+                <Typography>{accommodation.accommodationName}</Typography>
               </Stack>
             </Grid>
 
@@ -69,7 +58,7 @@ const DriverDetails = () => {
                 <Typography variant="subtitle1" fontWeight="bold">
                   Số điện thoại
                 </Typography>
-                <Typography>{driver.phoneNumber}</Typography>
+                <Typography>{accommodation.phoneNumber}</Typography>
               </Stack>
             </Grid>
 
@@ -80,22 +69,40 @@ const DriverDetails = () => {
             <Grid item xs={12}>
               <Stack spacing={2}>
                 <Typography variant="subtitle1" fontWeight="bold">
-                  Loại xe
+                  Địa chỉ
                 </Typography>
-                <Typography>{getVehicleTypeDisplay(driver.vehicleType)}</Typography>
+                <Typography>{accommodation.address}</Typography>
               </Stack>
             </Grid>
 
-            {driver.imgs && driver.imgs.length > 0 && (
+            <Grid item xs={12}>
+              <Stack spacing={2}>
+                <Typography variant="subtitle1" fontWeight="bold">
+                  Mô tả
+                </Typography>
+                <Typography>{accommodation.accommodationDescription || 'Không có mô tả'}</Typography>
+              </Stack>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Stack spacing={2}>
+                <Typography variant="subtitle1" fontWeight="bold">
+                  Số sao
+                </Typography>
+                <Typography>{accommodation.star || 'Chưa đánh giá'}</Typography>
+              </Stack>
+            </Grid>
+
+            {accommodation.imgs && accommodation.imgs.length > 0 && (
               <Grid item xs={12}>
                 <Stack spacing={2}>
                   <Typography variant="subtitle1" fontWeight="bold">
                     Hình ảnh
                   </Typography>
                   <Grid container spacing={2}>
-                    {driver.imgs.map((img, index) => (
+                    {accommodation.imgs.map((img, index) => (
                       <Grid item key={index} xs={12} sm={6} md={4}>
-                        <img src={img} alt={`Driver ${index + 1}`} style={{ width: '100%', borderRadius: 8 }} />
+                        <img src={img} alt={`Accommodation ${index + 1}`} style={{ width: '100%', borderRadius: 8 }} />
                       </Grid>
                     ))}
                   </Grid>
@@ -109,4 +116,4 @@ const DriverDetails = () => {
   );
 };
 
-export default DriverDetails;
+export default AccommodationDetails;
